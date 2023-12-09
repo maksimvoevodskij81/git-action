66f6b7e86502e442956658a2ac0fafdd421d48b4
maksym <maksimvoevodskij81@gmail.com>
2023-12-09 10:31:47 +0100

    add db changes to db

---
+++ PieApp/Migrations/20231209093000_Titel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PieApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Titel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Categories");
+        }
+    }
+}
