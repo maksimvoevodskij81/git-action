fdc5457db9d56b62ce7f28df0503407e5d2834e1
maksym <maksimvoevodskij81@gmail.com>
2023-12-11 16:08:50 +0100

    remove title

---
+++ PieApp/Migrations/20231211150709_RemoveTitle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PieApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Categories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
